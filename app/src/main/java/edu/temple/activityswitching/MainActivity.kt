8ea706a4f6d19f9763090bfe649594fd25f1af01
Sam Sepial <zprestoz@protonmail.com>
2022-03-06 18:35:14 -0500

    Added all of the photos to be used for the recyclerview layout, as well as set up the main activity, instanciating the photoadapter class.

--- app/src/main/java/edu/temple/activityswitching/MainActivity.kt
+++ app/src/main/java/edu/temple/activityswitching/MainActivity.kt
@@ -2,10 +2,21 @@ package edu.temple.activityswitching
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var photoAdapter: PhotoAdapter
+    private var dataList = mutableListOf<PhotoObject>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = GridLayoutManager(applicationContext, 4)
+        photoAdapter = PhotoAdapter(applicationContext)
+        recyclerView.adapter = photoAdapter
     }
 }
