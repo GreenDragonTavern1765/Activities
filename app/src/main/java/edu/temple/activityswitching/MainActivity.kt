94aba4dc68a09fa4c382b167715385a73306fb80
Sam Sepial <zprestoz@protonmail.com>
2022-03-06 19:06:24 -0500

    Added more photos and added them to the data list. Ran it and it works fine, now the next part is to add the onclicklistener() to take the user to the next activity.

--- app/src/main/java/edu/temple/activityswitching/MainActivity.kt
+++ app/src/main/java/edu/temple/activityswitching/MainActivity.kt
@@ -18,5 +18,38 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = GridLayoutManager(applicationContext, 4)
         photoAdapter = PhotoAdapter(applicationContext)
         recyclerView.adapter = photoAdapter
+        fillDataList()
+        photoAdapter.setDataList(dataList)
+    }
+
+    fun fillDataList() {
+        dataList.add(PhotoObject("Arryn","The Eyrie", R.drawable.arryn))
+        dataList.add(PhotoObject("Baratheon","Dragonstone", R.drawable.stannis))
+        dataList.add(PhotoObject("Baratheon","Storm's End", R.drawable.baratheon))
+        dataList.add(PhotoObject("Blackfyre","Kings Landing", R.drawable.blackfyre))
+        dataList.add(PhotoObject("Bolton","Dreadfort", R.drawable.bolton))
+        dataList.add(PhotoObject("Cerwyn","Castle Cerwyn", R.drawable.cerwyn))
+        dataList.add(PhotoObject("Clegane","Clegane Keep", R.drawable.clegane))
+        dataList.add(PhotoObject("Corbray","Hearts Home", R.drawable.corbray))
+        dataList.add(PhotoObject("Dayne","Starfall", R.drawable.dayne))
+        dataList.add(PhotoObject("Frey","The Twins", R.drawable.frey))
+        dataList.add(PhotoObject("Glover","Deepwood Mott", R.drawable.glover))
+        dataList.add(PhotoObject("Grafton","Gulltown", R.drawable.grafton))
+        dataList.add(PhotoObject("Greyjoy","Iron Islands", R.drawable.greyjoy))
+        dataList.add(PhotoObject("Hightower","Oldtown", R.drawable.hightower))
+        dataList.add(PhotoObject("Karstark","Karhold", R.drawable.karstark))
+        dataList.add(PhotoObject("Lannister","Casterly Rock", R.drawable.lannister))
+        dataList.add(PhotoObject("Mallister", "Seagard", R.drawable.mallister))
+        dataList.add(PhotoObject("Martell","Dorne", R.drawable.martell))
+        dataList.add(PhotoObject("Mormont","Bear Island", R.drawable.mormont))
+        dataList.add(PhotoObject("Royce","Runestone", R.drawable.royce))
+        dataList.add(PhotoObject("Stark","Winterfell", R.drawable.stark))
+        dataList.add(PhotoObject("Targaryen","Dragonstone", R.drawable.targaryen))
+        dataList.add(PhotoObject("Tarly","Horn Hill", R.drawable.tarly))
+        dataList.add(PhotoObject("Tarth","Tarth", R.drawable.tarth))
+        dataList.add(PhotoObject("Tollett","Grey Glen", R.drawable.tollett))
+        dataList.add(PhotoObject("Tully","Riverrun", R.drawable.tully))
+        dataList.add(PhotoObject("Tyrell","Highgarden", R.drawable.tyrell))
+        dataList.add(PhotoObject("Umber","Last Hearth", R.drawable.umber))
     }
 }
