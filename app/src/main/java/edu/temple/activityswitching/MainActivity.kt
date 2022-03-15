7a64e2d1f4cd724667461866497c9ed7881c7569
Sam Sepial <zprestoz@protonmail.com>
2022-03-15 19:00:46 -0400

    Set the adapter to take in the resource string array rather than from the main activity. It works properly, taking in from the string.xml

--- app/src/main/java/edu/temple/activityswitching/MainActivity.kt
+++ app/src/main/java/edu/temple/activityswitching/MainActivity.kt
@@ -1,9 +1,11 @@
 package edu.temple.activityswitching
 
 import android.content.Intent
+import android.content.res.Resources
 import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -11,7 +13,6 @@ import androidx.recyclerview.widget.RecyclerView
 class MainActivity : AppCompatActivity(), viewListener {
 
     private lateinit var photoAdapter: PhotoAdapter
-    private var dataList = mutableListOf<PhotoObject>()
     var name: String? = null
     var description: String? = null
     var image: Image? = null
@@ -24,39 +25,7 @@ class MainActivity : AppCompatActivity(), viewListener {
         recyclerView.layoutManager = GridLayoutManager(applicationContext, 4)
         photoAdapter = PhotoAdapter(applicationContext, this)
         recyclerView.adapter = photoAdapter
-        fillDataList()
-        photoAdapter.setDataList(dataList)
-    }
-
-    fun fillDataList() {
-        dataList.add(PhotoObject("Arryn","The Eyrie", R.drawable.arryn))
-        dataList.add(PhotoObject("Baratheon","Dragonstone", R.drawable.stannis))
-        dataList.add(PhotoObject("Baratheon","Storm's End", R.drawable.baratheon))
-        dataList.add(PhotoObject("Blackfyre","Kings Landing", R.drawable.blackfyre))
-        dataList.add(PhotoObject("Bolton","Dreadfort", R.drawable.bolton))
-        dataList.add(PhotoObject("Cerwyn","Castle Cerwyn", R.drawable.cerwyn))
-        dataList.add(PhotoObject("Clegane","Clegane Keep", R.drawable.clegane))
-        dataList.add(PhotoObject("Corbray","Hearts Home", R.drawable.corbray))
-        dataList.add(PhotoObject("Dayne","Starfall", R.drawable.dayne))
-        dataList.add(PhotoObject("Frey","The Twins", R.drawable.frey))
-        dataList.add(PhotoObject("Glover","Deepwood Mott", R.drawable.glover))
-        dataList.add(PhotoObject("Grafton","Gulltown", R.drawable.grafton))
-        dataList.add(PhotoObject("Greyjoy","Iron Islands", R.drawable.greyjoy))
-        dataList.add(PhotoObject("Hightower","Oldtown", R.drawable.hightower))
-        dataList.add(PhotoObject("Karstark","Karhold", R.drawable.karstark))
-        dataList.add(PhotoObject("Lannister","Casterly Rock", R.drawable.lannister))
-        dataList.add(PhotoObject("Mallister", "Seagard", R.drawable.mallister))
-        dataList.add(PhotoObject("Martell","Dorne", R.drawable.martell))
-        dataList.add(PhotoObject("Mormont","Bear Island", R.drawable.mormont))
-        dataList.add(PhotoObject("Royce","Runestone", R.drawable.royce))
-        dataList.add(PhotoObject("Stark","Winterfell", R.drawable.stark))
-        dataList.add(PhotoObject("Targaryen","Dragonstone", R.drawable.targaryen))
-        dataList.add(PhotoObject("Tarly","Horn Hill", R.drawable.tarly))
-        dataList.add(PhotoObject("Tarth","Tarth", R.drawable.tarth))
-        dataList.add(PhotoObject("Tollett","Grey Glen", R.drawable.tollett))
-        dataList.add(PhotoObject("Tully","Riverrun", R.drawable.tully))
-        dataList.add(PhotoObject("Tyrell","Highgarden", R.drawable.tyrell))
-        dataList.add(PhotoObject("Umber","Last Hearth", R.drawable.umber))
+        photoAdapter.setDataList()
     }
 
     override fun onItemClick(photo: PhotoObject) {
--- app/src/main/java/edu/temple/activityswitching/PhotoAdapter.kt
+++ app/src/main/java/edu/temple/activityswitching/PhotoAdapter.kt
@@ -2,6 +2,7 @@ package edu.temple.activityswitching
 
 import android.content.Context
 import android.content.Intent
+import android.media.Image
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -17,10 +18,51 @@ interface viewListener {
 }
 
 class PhotoAdapter(var context : Context, val listener: viewListener) : RecyclerView.Adapter<PhotoAdapter.ViewHolder>() {
-    var dataList = emptyList<PhotoObject>()
+    private var dataList = mutableListOf<PhotoObject>()
+    private var houseNames = context.resources.getStringArray(R.array.houseNames)
+    private var houseSeats = context.resources.getStringArray(R.array.houseSeats)
+    private var houseSigils = mutableListOf<Int>()
 
-    internal fun setDataList(dataList: List<PhotoObject>) {
-        this.dataList = dataList
+    internal fun setSigilList() {
+        houseSigils.add(R.drawable.arryn)
+        houseSigils.add(R.drawable.stannis)
+        houseSigils.add(R.drawable.baratheon)
+        houseSigils.add(R.drawable.blackfyre)
+        houseSigils.add(R.drawable.bolton)
+        houseSigils.add(R.drawable.cerwyn)
+        houseSigils.add(R.drawable.clegane)
+        houseSigils.add(R.drawable.corbray)
+        houseSigils.add(R.drawable.dayne)
+        houseSigils.add(R.drawable.frey)
+        houseSigils.add(R.drawable.glover)
+        houseSigils.add(R.drawable.grafton)
+        houseSigils.add(R.drawable.greyjoy)
+        houseSigils.add(R.drawable.hightower)
+        houseSigils.add(R.drawable.karstark)
+        houseSigils.add(R.drawable.lannister)
+        houseSigils.add(R.drawable.mallister)
+        houseSigils.add(R.drawable.martell)
+        houseSigils.add(R.drawable.mormont)
+        houseSigils.add(R.drawable.royce)
+        houseSigils.add(R.drawable.stark)
+        houseSigils.add(R.drawable.targaryen)
+        houseSigils.add(R.drawable.tarly)
+        houseSigils.add(R.drawable.tarth)
+        houseSigils.add(R.drawable.tollett)
+        houseSigils.add(R.drawable.tully)
+        houseSigils.add(R.drawable.tyrell)
+        houseSigils.add(R.drawable.umber)
+    }
+
+    internal fun setDataList() {
+        setSigilList()
+        for (i in 0..27) {
+            var temp = PhotoObject(
+                houseNames[i],
+                houseSeats[i],
+                houseSigils[i])
+            dataList.add(temp)
+        }
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -54,3 +96,5 @@ class PhotoAdapter(var context : Context, val listener: viewListener) : Recycler
         return dataList.size
     }
 }
+
+
